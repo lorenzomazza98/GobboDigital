39ac80156939925e71f22b7167f3b187144a00fb
Lorenzo Mazza <lorenzo.mazza001@studenti.uniparthenope.it>
2024-12-09 16:20:54 +0100

    Add existing project

---
+++ GobboDigital/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  GobboDigital
+//
+//  Created by Lorenzo Mazza on 09/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+Text("HI")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GobboDigital/GobboDigitalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GobboDigitalApp.swift
+//  GobboDigital
+//
+//  Created by Lorenzo Mazza on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GobboDigitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
